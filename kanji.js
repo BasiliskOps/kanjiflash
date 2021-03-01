66dc568bb80d2fa036de6446682eec7906ff711d
BasiliskOps <noro.nero@gmail.com>
2021-03-01 12:30:35 -0600

    intial commit: global js variables

---
+++ kanji.js
@@ -0,0 +1,4 @@
+const kanji = document.querySelector('.kanji')
+const meaning = document.querySelector('.meaning')
+const checkButton = document.querySelector('.check')
+const nextButton = document.querySelector('.next')
