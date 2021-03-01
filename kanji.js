49320f9c5a614e4120cfd46b05f25b87bc560d01
BasiliskOps <noro.nero@gmail.com>
2021-03-01 12:46:32 -0600

    added japanese vocabulary

--- kanji.js
+++ kanji.js
@@ -2,3 +2,12 @@ const kanji = document.querySelector('.kanji')
 const meaning = document.querySelector('.meaning')
 const checkButton = document.querySelector('.check')
 const nextButton = document.querySelector('.next')
+
+words = {
+    死: 'Death',
+    再生: 'Rebirth',
+    幽霊: 'Haunted',
+    勝利: 'Victory',
+    雪辱: 'Revenge',
+    悪魔: 'Demon'
+}
